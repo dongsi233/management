5d003d7ad1d99e326f1d964f7a5e75bffc1d6c18
dongsi233 <13502268327@163.com>
2018-07-04 14:09:49 +0800

    loading

---
+++ src/components/loading/index.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="loading">
+        <div v-for="(dotNum, index) in dotNums" :key="index" :style="dotTransform(index, dotNums)">
+            <span :style="dotAimation(index, dotNums)"></span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'loading',
+    props: {
+        loadingRadiusVal: {
+            type: Number,
+            required: true,
+            default: 168
+        },
+        dotRadiusVal: {
+            type: Number,
+            required: true,
+            default: 24
+        },
+        // dotColorVal: {
+        //     type: String,
+        //     required: true,
+        //     default: '#d8d8d8'
+        // },
+        dotNums: {
+            type: Number,
+            required: true,
+            default: 10
+        }
+    },
+    computed: {
+        changeStyle: function() {
+            let rootEle = document.documentElement;
+            console.log(rootEle)
+            rootEle.style.setProperty('--loadingRadius', `${this.loadingRadius}px`)
+            rootEle.style.setProperty('--dotRadius', `${this.dotRadius}px`)
+            rootEle.style.setProperty('--dotColor', this.dotColor)
+        }
+    },
+    methods: {
+        dotTransform: function(index, dotNums) {
+            let rad = 2 * Math.PI / dotNums  * index;
+            let dotX =  Math.cos(rad) * this.loadingRadiusVal / 2;
+            let dotY =  Math.sin(rad) * this.loadingRadiusVal / 2;
+            return {
+                transform: `translate(${dotX}px,${dotY}px)`
+            };
+        },
+        dotAimation: function(index, dotNums) {
+            let delayTime = `${-1 * (1 + (index + 1) * 1 / dotNums) }s`
+            return {
+                animationDelay: delayTime
+            }
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+   :root {
+    --loadingRadius: 168px;
+    --dotRadius: 24px;
+    --dotColor: #d8d8d8;
+    }
+
+    .loading{
+        position: relative;
+        font-size: 0;
+        border-radius: 50%;
+        transform-origin: center;
+        
+        width: var(--loadingRadius);
+        height: var(--loadingRadius);
+        color: var(--dotColor);
+        
+        div {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            border-radius: 100%;
+
+            width: var(--dotRadius);
+            height: var(--dotRadius);
+            color: var(--dotColor);
+            margin-top: calc((var(--dotRadius) / 2) * -1);
+            margin-left: calc((var(--dotRadius) / 2) * -1);
+            
+        }
+        
+        span {
+            display: block;
+            background-color: currentColor;
+            border: 0 solid currentColor;
+            border-radius: 100%;
+            animation: ball-spin 1s infinite ease-in-out;  
+            
+            width: var(--dotRadius);
+            height: var(--dotRadius);
+        }
+    }
+
+    // animation keyframes
+
+    @keyframes ball-spin {
+        0%,
+        100% {
+        opacity: 1;
+        transform: scale(1);
+        }
+        20% {
+        opacity: 1;
+        }
+        80% {
+        opacity: 0;
+        transform: scale(0);
+        }
+    }
+</style>
+
+
--- src/views/index/components/table.vue
+++ src/views/index/components/table.vue
@@ -1,17 +1,59 @@
 <template>
     <div>
         <el-button v-waves="{color:'yellow'}" type="primary" >水波纹效果</el-button>
+
+        <el-card :body-style="{ padding: '0px' }">
+            <div slot="header">
+                <span><!-- card title --></span>
+            </div>
+            <css-doodle>
+                :doodle { 
+                    @grid: 1x10 / 61.8vmin; 
+                }
+
+                @place-cell: center; 
+                @size: calc(@index() * 10%);
+                
+                border-radius: 50%;
+                border-style: dashed;
+                border-width: calc(@index() * 1vmin); 
+                border-color: hsla(
+                    calc(20 * @index()), 70%, 68%, 
+                    calc(3 / @index() * .8)
+                );
+                
+                --d: @rand(20s, 40s); 
+                --rf: @rand(360deg);
+                --rt: calc(var(--rf) + @pick(1turn, -1turn));
+
+                animation: spin var(--d) linear infinite;
+                @keyframes spin {
+                    from { transform: rotate(var(--rf)) }
+                    to   { transform: rotate(var(--rt)) }
+                }
+            </css-doodle> 
+        </el-card>
+        
+    <loading :loadingRadiusVal="200" :dotRadiusVal="24" :dotNums="10"></loading>
     </div>
 </template>
 <script>
 import waves from '@/directive/waves/index.js'
+import CSSDoole from 'css-doodle'
+import loading from '@/components/loading'
 
 export default {
+    components: {
+        loading
+    },
     directives: {
         waves
     }
 }
 </script>
 <style lang="scss" scoped>
-
+    .el-card {
+        background: #0a0c27;
+        overflow: hidden;
+    }
 </style>
